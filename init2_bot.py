925fc74cf3be3b0495e42c35b203ff30f90a309e
karmelyoei <karmel.salah@gmail.com>
2021-06-23 17:01:23 +0300

    some changes

--- init2_bot.py
+++ init2_bot.py
@@ -26,24 +26,55 @@ client.start(bot_token=Bot_TOKEN)
 
 
 
-@client.on(events.NewMessage(pattern='/start'))
-async def questions(event):
-    await event.respond('Hello! Talking to you from Telethon')
+# @client.on(events.NewMessage(pattern='/start'))
+# async def questions(event):
+#     await event.respond('Hello! Talking to you from Telethon')
+
+chat="@test2GossipPartnerBot" 
 
 @client.on(events.NewMessage(pattern='(?i)hi|hello'))
 async def handler(event):
-    await event.respond('Hey!, what is your favorite emoji?!')
+    # await event.respond('Hey!, what is your favorite emoji?!')
      # Good
-    chat = await event.get_chat()
+    # chat = await event.get_chat()
     sender = await event.get_sender()
     chat_id = event.chat_id
     sender_id = event.sender_id
+    # <you> denotes outgoing messages you sent
+    # <usr> denotes incoming response messages
+    async with event.client.conversation(chat) as conv:
+        # <you> Hi!
+        await conv.send_message(event.chat_id,'Hey!, what is your favorite emoji?!')
+
+        # <usr> Hello!
+        hello = await conv.get_response(0)
+
+        # <you> Please tell me your name
+        await conv.send_message(event.chat_id,'Please tell me your name')
+
+        # <usr> ?
+        name = await conv.get_response(0).raw_text
+
+        while not any(x.isalpha() for x in name):
+            # <you> Your name didn't have any letters! Try again
+            await conv.send_message(event.chat_id,"Your name didn't have any letters! Try again")
+
+            # <usr> Human
+            name = await conv.get_response(0)
+        print("response", name)
+            
+
+    # <you> Thanks Human!
+    await conv.send_message(event.chat_id,'Thanks {}!'.format(name))
     
     
 
 
-@client.on(events.NewMessage(pattern=r"(?:\u263a|\U0001f645)"))
-async def get_emoji(event):  
-  print(event.message)
+# @client.on(events.NewMessage(pattern=r"(?:\u263a|\U0001f645)"))
+# async def get_emoji(event):  
+#   print(event.message)
 
 client.run_until_disconnected()
+
+
+
